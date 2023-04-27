b682e6c6080ae49498939962abafde8d64d5cf8d
Matheus de Oliveira <ramos.matheus@engenharia.ufjf.br>
2023-04-27 15:31:28 -0300

    ajustado função align do markdown.py

--- src/markdown.py
+++ src/markdown.py
@@ -1,9 +1,9 @@
 import streamlit as st 
 
 
-st.markdown("<h3 style='text-align: center'> Quantidade de Restaurantes por Pais </h3>", unsafe_allow_html=True)
 def aling(h = 'h1',text = 'title'):
     corpo = "<" + h
-    corpo += ' style=' + 'text-align: center' + '> '
+    corpo += " style=" + "'text-align: center'>"
     corpo += text
-    return  st.markdown(corpo, unsafe_allow_html=True)
+    corpo += "</" + h + ">"
+    return  corpo
