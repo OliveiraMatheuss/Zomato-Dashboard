28ce793f605de534a16e8a2a849a275720786d0e
Matheus de Oliveira <ramos.matheus@engenharia.ufjf.br>
2023-04-27 15:04:45 -0300

    Criado o grafico 1 da visao paises

--- pages/1_countries.py
+++ pages/1_countries.py
@@ -8,7 +8,7 @@ from folium.plugins import MarkerCluster
 from millify import millify as mil
 from src import transformation as ts
 
-st.set_page_config(page_title= 'Home',
+st.set_page_config(page_title= 'Countries',
                     layout= 'wide')
 
 
@@ -32,3 +32,21 @@ select_country_mult = st.sidebar.multiselect(label ='Escolha os Paises que Desej
 # filtros
 
 select_country = df1[df1['country_name'].isin(select_country_mult)]
+
+#==============================================================================================================#
+#====================================    LAYOUT      ==========================================================#
+#==============================================================================================================#
+
+st.header('🌎 Visão Paises')
+
+with st.container():
+    st.markdown("<h3 style='text-align: center'> Quantidade de Restaurantes por Pais </h3>", unsafe_allow_html=True)
+    cols = ['country_name', 'restaurant_id']
+    aux = df1.loc[:,cols].groupby('country_name').count().sort_values( by= 'restaurant_id',ascending=False).head(10).reset_index().head()
+    bar = px.bar(aux, x = 'country_name', y = 'restaurant_id',
+            labels= {
+                'country_name': 'Pais',
+                'restaurant_id': 'Qtd de Restaurantes'
+            }
+            )
+    st.plotly_chart(bar, use_container_width= True)
