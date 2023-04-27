f779799cb71cc80c8013644d20988e5c6fa5f850
Matheus de Oliveira <ramos.matheus@engenharia.ufjf.br>
2023-04-27 14:29:54 -0300

    Transferido funções de limpeza e transfor para transformation.py

--- Home.py
+++ Home.py
@@ -10,6 +10,7 @@ from streamlit_folium import folium_static
 import inflection
 from folium.plugins import MarkerCluster
 from millify import millify as mil
+from src import transformation as ts
 
 st.set_page_config(page_title= 'Home',
                     layout= 'wide')
@@ -18,105 +19,6 @@ st.set_page_config(page_title= 'Home',
 #==========================================================================================================#
 #================================= FUNÇÕES ================================================================#
 #==========================================================================================================#
-
-def rename_columns(dataframe):
-    
-    """
-        renomea as colunas do dataset
-
-    """
-    df = dataframe.copy()
-    title = lambda x: inflection.titleize(x)
-    snakecase = lambda x: inflection.underscore(x)
-    spaces = lambda x: x.replace(" ", "")
-    cols_old = list(df.columns)
-    cols_old = list(map(title, cols_old))
-    cols_old = list(map(spaces, cols_old))
-    cols_new = list(map(snakecase, cols_old))
-    df.columns = cols_new
-    return df
-
-
-
-def create_price_tye(price_range):
-    if price_range == 1:
-        return "cheap"
-    elif price_range == 2:
-        return "normal"
-    elif price_range == 3:
-        return "expensive"
-    else:
-        return "gourmet"
-
-
-
-def cuisines_ratting(df, cuisine, ascending = True):
-    cols = ['restaurant_name', 'country_name', 'aggregate_rating']
-    df = df[df.cuisines.isin([cuisine])]
-    df = df[cols]
-
-    return df.sort_values('aggregate_rating', ascending= ascending)
-
-def limpeza(df1):
-    
-    COLORS = {
-                "3F7E00": "darkgreen",
-                "5BA829": "green",
-                "9ACD32": "lightgreen",
-                "CDD614": "orange",
-                "FFBA00": "red",
-                "CBCBC8": "darkred",
-                "FF7800": "darkred",
-                }
-    
-    COUNTRIES = {
-                    1: "India",
-                    14: "Australia",
-                    30: "Brazil",
-                    37: "Canada",
-                    94: "Indonesia",
-                    148: "New Zeland",
-                    162: "Philippines",
-                    166: "Qatar",
-                    184: "Singapure",
-                    189: "South Africa",
-                    191: "Sri Lanka",
-                    208: "Turkey",
-                    214: "United Arab Emirates",
-                    215: "England",
-                    216: "United States of America",
-                    }
-    
-    df1 = df1.copy()
-
-    # 0. Mudar nome das colunas
-    df1 = rename_columns(df1)
-    # 1. Transformar o tipo int para object na coluna "Restauran ID"
-    df1['restaurant_id'] = df1.loc[:,'restaurant_id'].astype(str)
-
-    #2 Remover valores ausentes nan
-    df1.dropna(inplace= True)
-
-    #3. categorizar por um tipo de culinaria
-    df1["cuisines"] = df1.loc[:, "cuisines"].apply(lambda x: x.split(",")[0])
-
-    #4. Remover valores duplicados
-    df1.drop_duplicates(inplace= True)
-
-    #5. Alterando variavel categorica price_range
-    df1['price_range'] = df1['price_range'].apply(lambda x: create_price_tye(x))
-
-    #6. Criando a coluna country_name
-    df1['country_name'] = df1['country_code'].map(COUNTRIES)
-
-    #7. Criando a coluna color_name
-    df1['rating_color_name'] = df1['rating_color'].map(COLORS)
-
-    #8. Removendo pratos que são iguais a zero
-    df1 = df1[(df1['average_cost_for_two'] != 0) & (df1['aggregate_rating'] !=0)]
-    
-    return df1
-
 
 def mapMaker(df):
     
@@ -148,7 +50,7 @@ def mapMaker(df):
 #===============================================================================================================#
 
 df = pd.read_csv("dataset/zomato.csv")
-df1 = limpeza(df)
+df1 = ts.limpeza(df)
 
 
 
