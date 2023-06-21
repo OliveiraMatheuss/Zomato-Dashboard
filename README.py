32863bc97ef0ec5bc61d0d9d47d120d86511c4b9
Matheus de Oliveira <ramos.matheus@engenharia.ufjf.br>
2023-06-21 13:18:16 -0300

    Resolvido Merge

--- README.py
+++ README.py
@@ -10,14 +10,8 @@ import streamlit.components.v1 as components
 
 with st.sidebar:
     components.html("""
-<<<<<<< HEAD
                     <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="oliveiramatheuss" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/oliveiramatheuss?trk=profile-badge">Matheus Oliveira</a></div>
-                    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-                           
-=======
-                    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="HORIZONTAL" data-vanity="oliveiramatheuss" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/oliveiramatheuss?trk=profile-badge">Matheus Oliveira</a></div>
-              
->>>>>>> f4390e7ffe20d3f62ceeb7981c75b8aac403c8a8
+                    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>              
               """, height= 310)
 
 
